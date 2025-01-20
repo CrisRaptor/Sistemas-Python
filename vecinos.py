f1b4e222a4f238b28cf053c03f8f99c32dcb58f5
CrisR <cristiangarcha7@gmail.com>
2025-01-20 08:46:12 +0100

    Inicio vecinos py

---
+++ vecinos.py
@@ -0,0 +1,12 @@
+#####     RULE SET     #######
+# En un tablero de 3x3 o 5x5 #
+# Se generan aleatoriamente vecinos '*' #
+
+# El juego simula rondas, en cada ronda, #
+
+# Los vecinos mueren o nacen segun las siguienes normas: #
+# Un vecino muere si hay 0,1,4+ vecinos alrededor (8 casillas) #
+# Un vecino sigue vivo si hay 2 o 3 vecinos alrededor #
+# Un vecino nace en un hueco si hay 3 vecinos exactos #
+
+# Las rondas pasan al presionar una tecla
